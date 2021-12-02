b4e8d0ae20476543fbc7804cb08c1dccf1085eb1
sz0gyaeve@vasvari.org <sz0gyaeve@vasvari.org>
2021-12-02 12:09:16 +0100

    Program megjavítása

--- Desktop/src/Vizsgaremek/Pages/WelcomePage.xaml.cs
+++ Desktop/src/Vizsgaremek/Pages/WelcomePage.xaml.cs
@@ -13,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Futóverseny
+namespace Futóverseny.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for WelcomePage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WelcomePage : UserControl
     {
-        public MainWindow()
+        public WelcomePage()
         {
             InitializeComponent();
         }
