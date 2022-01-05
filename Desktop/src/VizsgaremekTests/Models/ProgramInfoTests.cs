966f4aaba5cfe1cb79159a93138e5f9624455b5d
sz0gyaeve@vasvari.org <sz0gyaeve@vasvari.org>
2022-01-05 13:18:11 +0100

    Program adatok és tesztek jók

--- Desktop/src/VizsgaremekTests/Models/ProgramInfoTests.cs
+++ Desktop/src/VizsgaremekTests/Models/ProgramInfoTests.cs
@@ -17,13 +17,55 @@ namespace Vizsgaremek.Models.Tests
         {
             // arrange
             ProgramInfo programInfo = new ProgramInfo();
-            Version expected = new Version(0,0,3,0);
+            Version expected = new Version(0,0,4,0);
 
             // act
             Version actual = programInfo.Version;
 
             // assert
-            Assert.AreEqual(expected, actual, "Version is not 0.0.3.0");
+            Assert.AreEqual(expected, actual, "Version is not 0.0.4.0");
+        }
+
+        [TestMethod()]
+        public void ProgramInfoTestTitle()
+        {
+            // arrange
+            ProgramInfo programInfo = new ProgramInfo();
+            string expected = "Vizsgaremek";
+
+            // act
+            string actual = programInfo.Title;
+
+            // assert
+            Assert.AreEqual(expected, actual, "Vizsgaremek");
+        }
+
+        [TestMethod()]
+        public void ProgramInfoTestDescription()
+        {
+            // arrange
+            ProgramInfo programInfo = new ProgramInfo();
+            string expected = "Programnév teszt";
+
+            // act
+            string actual = programInfo.Description;
+
+            // assert
+            Assert.AreEqual(expected, actual, "Programnév teszt");
+        }
+
+        [TestMethod()]
+        public void ProgramInfoTestCompany()
+        {
+            // arrange
+            ProgramInfo programInfo = new ProgramInfo();
+            string expected = "Vasvári";
+
+            // act
+            string actual = programInfo.Company;
+
+            // assert
+            Assert.AreEqual(expected, actual, "Vasvári");
         }
     }
 }
