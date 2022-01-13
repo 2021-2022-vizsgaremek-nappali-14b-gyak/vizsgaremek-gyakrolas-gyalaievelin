57f74d274f9ec3c586f87518b0713d6a163729c8
sz0gyaeve@vasvari.org <sz0gyaeve@vasvari.org>
2022-01-13 08:36:44 +0100

    TDD van működő kód

--- Desktop/src/Vizsgaremek/ViewModels/DatabaseSourceViewModel.cs
+++ Desktop/src/Vizsgaremek/ViewModels/DatabaseSourceViewModel.cs
@@ -5,19 +5,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Vizsgaremek.Repositories;
+using Vizsgaremek.Models;
 
 namespace Vizsgaremek.ViewModels
 {
-    class DatabaseSourceViewModel
+    public class DatabaseSourceViewModel
     {
         private ObservableCollection<string> displayedDatabaseSources;
         private string selectedDatabaseSource;
         DatabaseSources repoDatabaseSources;
+        private DbSource dbSource;
+    
 
         public DatabaseSourceViewModel()
         {
             repoDatabaseSources = new DatabaseSources();
             displayedDatabaseSources = new ObservableCollection<string>(repoDatabaseSources.GetAllDatabaseSources());
+        }
+
+        public DbSource DbSource
+        {
+            get
+            {
+                return DbSource.NONE;
+            }
         }
 
         public ObservableCollection<string> DisplayedDatabaseSource { get => displayedDatabaseSources; set => displayedDatabaseSources = value; }
