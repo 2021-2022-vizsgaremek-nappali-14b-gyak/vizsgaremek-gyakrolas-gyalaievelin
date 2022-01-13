750185d8c9cf21fe1e367be9890425463d275d37
sz0gyaeve@vasvari.org <sz0gyaeve@vasvari.org>
2022-01-13 08:43:41 +0100

    Egy teszteset kész

---
+++ Desktop/src/VizsgaremekTests/ViewModels/DatabaseSourceViewModelTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vizsgaremek.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vizsgaremek.Models;
+
+namespace Vizsgaremek.ViewModels.Tests
+{
+    [TestClass()]
+    public class DatabaseSourceViewModelTests
+    {
+        [TestMethod()]
+        public void DatabaseSourceViewModelTestLocalhost()
+        {
+            DatabaseSourceViewModel databaseSourceViewModel = new DatabaseSourceViewModel();
+            //A felhasználó rákattint a localhost szóra
+            databaseSourceViewModel.SelectedDatabaseSource = "localhost";
+            DbSource expectedDbSource = DbSource.LOCALHOST;
+
+            DbSource actualDbSource = databaseSourceViewModel.DbSource;
+
+            Assert.AreEqual(expectedDbSource, actualDbSource, "A kiválasztott adatbázis a 'localhost', de nem váltott át DbSource.LOCALHOST-ra");
+
+        }
+
+        [TestMethod()]
+        public void DatabaseSourceViewModelTestDevops()
+        {
+            DatabaseSourceViewModel databaseSourceViewModel = new DatabaseSourceViewModel();
+            //A felhasználó rákattint a localhost szóra
+            databaseSourceViewModel.SelectedDatabaseSource = "localhost";
+            DbSource expectedDbSource = DbSource.LOCALHOST;
+
+            DbSource actualDbSource = databaseSourceViewModel.DbSource;
+
+            Assert.AreEqual(expectedDbSource, actualDbSource, "A kiválasztott adatbázis a 'localhost', de nem váltott át DbSource.LOCALHOST-ra");
+
+        }
+
+        [TestMethod()]
+        public void DatabaseSourceViewModelTestNONE()
+        {
+            DatabaseSourceViewModel databaseSourceViewModel = new DatabaseSourceViewModel();
+            //A felhasználó rákattint a localhost szóra
+            databaseSourceViewModel.SelectedDatabaseSource = "localhost";
+            DbSource expectedDbSource = DbSource.LOCALHOST;
+
+            DbSource actualDbSource = databaseSourceViewModel.DbSource;
+
+            Assert.AreEqual(expectedDbSource, actualDbSource, "A kiválasztott adatbázis a 'localhost', de nem váltott át DbSource.LOCALHOST-ra");
+
+        }
+    }
+}
